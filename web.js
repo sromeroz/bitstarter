7375682f90ef3bfc9c140b8f51af9cb1ecf7afd5
Sergio Romero Zambrano <sromero@me.com>
2013-07-10 01:29:27 +0000

    Fixed bugs in web.js

--- web.js
+++ web.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var fs = require('fs');
+var buf = new Buffer(fs.readFileSync('index.html'),'utf-8');
+
 
 var app = express.createServer(express.logger());
-var buf = fs.readFileSync('index.html');
 var str = buf.toString();
 app.get('/', function(request, response) {
   response.send(str);
